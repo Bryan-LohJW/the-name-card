bb833ab252c44f515ca325f67382f04fef60ccde
Bryan Personal <bryanlohjw97@gmail.com>
2024-01-04 21:00:39 +0800

    feat: header component

---
+++ the-name-card/src/components/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import './Landing.scss';
+import { Header } from '..';
+
+const Landing = () => {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
+
+export default Landing;
--- the-name-card/src/main.tsx
+++ the-name-card/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import { Landing } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<div className="margins">
+			<div className="content">
+				<Landing />
+			</div>
+		</div>
+	</React.StrictMode>
+);
