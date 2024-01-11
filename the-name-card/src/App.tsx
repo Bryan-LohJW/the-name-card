419a09e7cfbc6e6c7ee15a5b13b7a3627c2a2b97
Bryan Personal <bryanlohjw97@gmail.com>
2024-01-11 20:55:28 +0800

    feat: implement react router

--- the-name-card/src/App.tsx
+++ the-name-card/src/App.tsx
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Landing, Margin } from './components';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Landing />,
+	},
+]);
+
 function App() {
-	return <></>;
+	return (
+		<Margin>
+			<RouterProvider router={router} />
+		</Margin>
+	);
 }
 
 export default App;
--- the-name-card/src/main.tsx
+++ the-name-card/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from './App';
 import './index.scss';
-import { Landing, Margin } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Margin>
-			<Landing />
-		</Margin>
+		<App />
 	</React.StrictMode>
 );
